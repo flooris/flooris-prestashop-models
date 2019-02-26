bae979549a412b206901ff96c4a30d411b17f2dc
Roan Brouwer <roan@flooris.nl>
2019-02-26 11:47:51 +0100

    Update ProductAttributeShop.php

--- src/Models/Product/ProductAttributeShop.php
+++ src/Models/Product/ProductAttributeShop.php
@@ -7,8 +7,21 @@ use Flooris\Prestashop\Models\PrestashopModel;
 
 class ProductAttributeShop extends PrestashopModel
 {
-    protected $table = 'product_attribute_shop';
     public $timestamps = false;
+
+    protected $table = 'product_attribute_shop';
+
+    protected $guarded = [];
+
+    // Set default values for attributes that are rarely filled.
+    protected $attributes = [
+        'wholesale_price'   => 0,
+        'ecotax'            => 0,
+        'weight'            => 0,
+        'unit_price_impact' => 0,
+        'default_on'        => null,
+        'available_date'    => '0000-00-00',
+    ];
 
     public function product()
     {
