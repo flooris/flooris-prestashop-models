edf13de154f6cc19b18e663df51c115f97943eaa
Jasper <j.stolwijk@flooris.nl>
2024-05-28 14:28:54 +0200

    Fixed ProductAttributeShop Primary Key model.
    - Added composite key as primary key.

--- src/Models/Product/ProductAttributeShop.php
+++ src/Models/Product/ProductAttributeShop.php
@@ -20,6 +20,13 @@ class ProductAttributeShop extends PrestashopModel
     protected $table = 'product_attribute_shop';
 
     /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = ['id_product_attribute', 'id_lang'];
+
+    /**
      * Indicates if the model should be timestamped.
      *
      * @var bool
